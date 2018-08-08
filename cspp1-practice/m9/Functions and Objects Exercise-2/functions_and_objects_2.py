f69d5027492e716dd875cc24df82fd3e85d9098b
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-08 20:57:09 +0530

    fun obj 2

--- cspp1-practice/m9/Functions and Objects Exercise-2/functions_and_objects_2.py
+++ cspp1-practice/m9/Functions and Objects Exercise-2/functions_and_objects_2.py
@@ -5,15 +5,18 @@
 def apply_to_each(L, f):
     for i in range(len(L)):
         L[i] = f(L[i])
-        return L + 1
+    return L
+def inc(a):
+    return a+1
 
 def main():
+
     data = input()
     data = data.split()
     list1 = []
     for j in data:
         list1.append(int(j))
-    print(apply_to_each(list1, int))
+    print(apply_to_each(list1, inc))
 
 if __name__ == "__main__":
     main()
